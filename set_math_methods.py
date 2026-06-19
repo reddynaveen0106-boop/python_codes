b4cf2c2e0105709789025ce5dc8b23ac0876e9a4
reddynaveen0106 <reddynaveen0106@gmail.com>
2026-06-19 18:37:30 +0530

    set math methods

---
+++ set_math_methods.py
@@ -0,0 +1,14 @@
+a = {10,20,30,40}
+b = {30,40,50,60}
+#Mathematical Operations
+#print(a.union(b))
+#print(a | b)
+
+print(a.intersection(b))
+print(a & b)
+
+print(a.difference(b))
+print( a-b)
+print(b-a)
+
+print(a.symmetric_difference(b))
