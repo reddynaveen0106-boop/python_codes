4988dbac6638bdeccf2a7e90a5d266f6f3a341c0
reddynaveen0106 <reddynaveen0106@gmail.com>
2026-06-19 11:17:24 +0530

    used map iterstor in iterate.py file line 19

--- iterat.py
+++ iterat.py
@@ -3,8 +3,19 @@ new_list = []
 for l in letters:
     new_list.append(l.upper())
     print(new_list)
+    
 # Enumerate
 letters = ['a', '', '']
 print(list(enumerate(letters, start = 1)))
 for index, value in enumerate(letters):
     print(index, value)
+
+letters = ['a', 'b', 'c']
+print(list(map(str.upper, letters)))
+
+numbers = ['1', '2', '3']
+print(list(map(int, numbers)))
+
+names = ['naveen', 'reddy', 'Kumar']
+for n in map(str.strip, names):
+    print(n)
