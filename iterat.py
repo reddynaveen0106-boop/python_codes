2c2c418f1108e27b8ec019c23321a31c8639c1f1
reddynaveen0106 <reddynaveen0106@gmail.com>
2026-06-19 14:12:11 +0530

    filtering the list in line no: 50

--- iterat.py
+++ iterat.py
@@ -22,3 +22,34 @@ for n in map(str.strip, names):
 
 letters = ['a', '', 'b',None, 'c', False]
 print(list(filter(bool, letters)))
+
+# lambda
+multiple = lambda x: x*2
+print(multiple(2))
+
+add = lambda x, y: x + y
+print(add(1,2))
+
+check = lambda i: i in "python"
+print(check('z'))
+
+# Lambda + Map
+
+price = ['$12.58','$123.276', '123.456']
+print(list(map(lambda p: float(p.replace("$", '')), price))) 
+               #2->put in Lambda  #->1 Data transformation 
+           #3 Map the function to iterartor to manipulate my data, All these things are related to abouve line 37 
+
+           
+# Lambda + filter 
+
+price = [120,30,300,80] 
+
+print(list(filter(lambda p: p >= 100, price)))
+
+students = [['Maria', 85],
+            ['kumar', 90],
+            ['Max', 60]]
+print(students [0][1] > 70)
+
+print(list(filter(lambda row: row[1] > 70, students)))
