677595712a56ee769c791a64b0888bceee255360
reddynaveen0106 <reddynaveen0106@gmail.com>
2026-06-20 11:05:04 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,11 @@
+my_dict = {
+    'a' : 10,
+    'b' : 20,
+    'c' : 20,
+    'a' : 40
+}
+
+print(my_dict) # Oredred
+#Keys are Unique
+#Values allow Duplicates
+print(my_dict['b']) #Not Indexed
---
+++ methods_in_dict.py
@@ -0,0 +1,7 @@
+# Methods
+
+user = {"id":1, "age": 30, "city": "berlin"}
+
+#Access
+print(user["age"])
+print(user.get("age"))
