3d9d089ce7b207efe8391fb7cf9059d923ea9fc6
reddynaveen0106 <reddynaveen0106@gmail.com>
2026-06-20 17:23:16 +0530

    functions

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-def going_to_gym():
+'''def going_to_gym():
     print("Ate Boiled eggs")
     print("wearing the shoes")
     print("walking to the gym")
@@ -33,4 +33,24 @@ print("Day->>>>>>>>>>>>>>>>>>>3")
 going_to_gym()
 study_plan()
 diet_plan()
-sleep_plan()
+sleep_plan()'''
+
+'''import math'''
+
+'''print(len("Python"))
+number = 4.2
+print(math.ceil(number))'''
+'''def greet():
+    print("Hello")
+greet()
+def ms_dhoni():
+    print("MS Dhoni is a GOAT in Indian cricket and he is a well known player across the and he is a proven finsher and only captain with all three format WC tropies i.e., t20i,champions trophy and the ODI wc")
+ms_dhoni()'''
+
+def clean_name(name):
+    cleaned = name.strip().lower()
+    print(cleaned)
+
+clean_name(" MkJkJkKjIKN   juJn kjj  jJJ JJiUWUEYDHBDH")
+clean_name("ehjdejhdejhejHDJSDJHS WDGEDHGHEDG HHJEFJ")
+clean_name("naveeN REDDY")
